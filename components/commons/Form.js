c8c898c986db74534e5c076b6ef2cab0458daa8f
German G Sayago <german.sayago@gmail.com>
2020-10-13 19:26:47 -0300

    se convirtio el form de class a function

--- components/commons/Form.js
+++ components/commons/Form.js
@@ -1,68 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Form extends Component {
-  render() {
-    return (
-      <>
-        <form>
-          <div className='row mb-1'>
-            <div className='col-sm-6'>
-              <div className='form-group'>
-                <input
-                  className='form-control '
-                  type='text'
-                  name='fname'
-                  placeholder='Nombre'
-                />
-              </div>
-            </div>
-            <div className='col-sm-6'>
-              <div className='form-group'>
-                <input
-                  className='form-control '
-                  type='text'
-                  name='lname'
-                  placeholder='Apellido'
-                />
-              </div>
-            </div>
-            <div className='col-md-6'>
-              <div className='form-group'>
-                <input
-                  className='form-control '
-                  type='text'
-                  name='email'
-                  placeholder='Email'
-                />
-              </div>
-            </div>
-            <div className='col-md-6'>
-              <div className='form-group'>
-                <input
-                  className='form-control '
-                  type='phone'
-                  name='phone'
-                  placeholder='Teléfono'
-                />
-              </div>
-            </div>
-            <div className='col'>
-              <div className='form-group'>
-                <textarea
-                  className='form-control '
-                  name='message'
-                  id=''
-                  rows='6'
-                  placeholder='Consulta'
-                ></textarea>
-              </div>
+export default function Form() {
+  return (
+    <>
+      <form>
+        <div className='row mb-1'>
+          <div className='col-sm-6'>
+            <div className='form-group'>
+              <input
+                className='form-control '
+                type='text'
+                name='fname'
+                placeholder='Nombre'
+              />
             </div>
           </div>
-          <button className='btn btn-primary'>Enviar mensaje</button>
-        </form>
-      </>
-    );
-  }
+          <div className='col-sm-6'>
+            <div className='form-group'>
+              <input
+                className='form-control '
+                type='text'
+                name='lname'
+                placeholder='Apellido'
+              />
+            </div>
+          </div>
+          <div className='col-md-6'>
+            <div className='form-group'>
+              <input
+                className='form-control '
+                type='text'
+                name='email'
+                placeholder='Email'
+              />
+            </div>
+          </div>
+          <div className='col-md-6'>
+            <div className='form-group'>
+              <input
+                className='form-control '
+                type='phone'
+                name='phone'
+                placeholder='Teléfono'
+              />
+            </div>
+          </div>
+          <div className='col'>
+            <div className='form-group'>
+              <textarea
+                className='form-control '
+                name='message'
+                id=''
+                rows='6'
+                placeholder='Consulta'
+              ></textarea>
+            </div>
+          </div>
+        </div>
+        <button className='btn btn-primary'>Enviar mensaje</button>
+      </form>
+    </>
+  );
 }
-
-export default Form;
